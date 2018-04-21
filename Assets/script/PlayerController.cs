4d37fe6a72cda11b288a845b39e3b7bcaf18995b
Eugene Pavlov <bjohn.ext@gmail.com>
2018-04-21 17:28:21 +0300

    add PlayerController.cs

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject floor;
+    public NavMeshAgent navAgent;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            var ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo)&& hitInfo.collider.name == floor.name)
+            {
+                Debug.LogFormat("hit {0} {1}", hitInfo.collider.name, hitInfo.point);
+                navAgent.destination = hitInfo.point;
+            }
+        }
+    }
+}
