bb483199e2ffb38011d9c4009c15515da1e48a6a
Eugene Pavlov <bjohn.ext@gmail.com>
2018-04-22 01:07:18 +0300

    add light detector

---
+++ Assets/BoyController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BoyController : MonoBehaviour
+{
+    public RenderTexture screen;
+    public Vector2Int screePixel;
+    public float lightLevel;
+    public Text lightText;
+
+
+    private Texture2D tex;
+
+    void Awake()
+    {
+        tex = new Texture2D(128, 128, TextureFormat.ARGB32, false);
+    }
+
+    void Update()
+    {
+        RenderTexture currentActive = RenderTexture.active;
+        RenderTexture.active = screen;
+        tex.ReadPixels(new Rect(0, 0, screen.width, screen.height), 0, 0);
+        tex.Apply();
+        RenderTexture.active = currentActive;
+        var color = tex.GetPixel(screePixel.x, screePixel.y);
+
+        lightText.gameObject.SetActive(color.grayscale >= lightLevel);
+        if (color.grayscale >= lightLevel)
+            lightText.color = new Color(1f,1f,1f, color.grayscale);
+    }
+}
--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -11,12 +11,14 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            //Debug.LogFormat("click {0} {1}", 128*Input.mousePosition.x/Screen.width, 128*Input.mousePosition.y/Screen.height);
+
             RaycastHit hitInfo;
             var ray = camera.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hitInfo)&& hitInfo.collider.name == floor.name)
             {
-                Debug.LogFormat("hit {0} {1}", hitInfo.collider.name, hitInfo.point);
+                //Debug.LogFormat("hit {0} {1}", hitInfo.collider.name, hitInfo.point);
                 navAgent.destination = hitInfo.point;
             }
         }
