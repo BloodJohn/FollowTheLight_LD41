95408fe2cc9f5eddde51d4937b75ce449152691a
Eugene Pavlov <bjohn.ext@gmail.com>
2018-04-22 19:54:45 +0300

    add intro scene

---
+++ Assets/script/IntroController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroController : MonoBehaviour
+{
+    public string firstLevelName;
+    private bool isLoading = false;
+
+    public void StartLevel()
+    {
+        if (isLoading) return;
+        isLoading = true;
+
+        SceneManager.LoadScene(firstLevelName);
+    }
+}
