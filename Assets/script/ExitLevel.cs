bcdeb41f1e24f1746cbe4e804449c24d8bbb16ca
Eugene Pavlov <bjohn.ext@gmail.com>
2018-04-22 19:21:36 +0300

    add ExitLevel

---
+++ Assets/script/ExitLevel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class ExitLevel : MonoBehaviour
+{
+    public float maxDist2 = 10f;
+    public NavMeshAgent boy;
+    public string nextSceneName;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dist2 = (boy.transform.position - transform.position).sqrMagnitude;
+
+        if (dist2 <= maxDist2 && !string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+            nextSceneName = string.Empty;
+        }
+    }
+}
